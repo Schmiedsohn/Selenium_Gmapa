34e04388d96b5139c5c4079953d82393ef9c5d43
Schmiedsohn <43189131+Schmiedsohn@users.noreply.github.com>
2023-07-06 21:00:21 +0200

    Add files via upload

--- Gmape.py
+++ Gmape.py
@@ -8,97 +8,81 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 
-# KOD
 
-while True:
-    cilj = input("trazim najduzu(d) ili najkracu(k)?")
-    if not (cilj == 'd' or cilj == 'k'):
-        print('odgovor mora biti d ili k')
-    else:
-        break
+def km_m(duzina):
+    d = float(duzina[:duzina.find(' ')])
+    if duzina.find(' km') == -1:
+        d = d / 1000
+    return d
 
-# otvaranje sajta
-driver = webdriver.Firefox()
-time.sleep(4)
-driver.get(pst.sajt_mape)
 
-try:
-    # klik na dugme putanja
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, pst.klik_putanja)))
-    driver.find_element(By.ID, pst.klik_putanja).click()
+def nadji_putanju(odakle, dokle, kakvu):
+    # otvaranje sajta
+    driver = webdriver.Firefox()
+    # driver = webdriver.Chrome()
+    time.sleep(4)
+    driver.get(pst.sajt_mape)
 
-    # popunjavanje polja odakle i dokle
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, pst.input_od)))
-    od = driver.find_element(By.XPATH, pst.input_od)
-    od.send_keys(pst.odakle)
+    try:
+        # klik na dugme putanja
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, pst.klik_putanja)))
+        driver.find_element(By.ID, pst.klik_putanja).click()
 
-    do = driver.find_element(By.XPATH, pst.input_do)
-    do.send_keys(pst.dokle)
-    do.send_keys(Keys.RETURN)
+        # popunjavanje polja odakle i dokle
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, pst.input_od)))
+        od = driver.find_element(By.XPATH, pst.input_od)
+        od.send_keys(odakle)
 
-    # odredjivanje vida pretrage - najbolje, auto, voz... bira auto
-    driver.find_element(By.XPATH, pst.klik_vid).click()
+        do = driver.find_element(By.XPATH, pst.input_do)
+        do.send_keys(dokle)
+        do.send_keys(Keys.RETURN)
 
-    # u opcijama izaberi da bude bez naplate
-    driver.find_element(By.XPATH, pst.klik_opcije).click()
+        # odredjivanje vida pretrage - najbolje, auto, voz... bira auto
+        driver.find_element(By.XPATH, pst.klik_vid).click()
 
-    nrampe = driver.find_element(By.XPATH, pst.klik_nrampe)
-    driver.execute_script("arguments[0].click();", nrampe)
+        # u opcijama izaberi da bude bez naplate
+        driver.find_element(By.XPATH, pst.klik_opcije).click()
 
-    # ocitavanje svih vremena predlozenih putanja u levom panou
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, pst.trajanje)))
-    pvoznje = driver.find_elements(By.XPATH, pst.pano_vremena)
-    pkilometara = driver.find_elements(By.XPATH, pst.pano_duzine)
+        nrampe = driver.find_element(By.XPATH, pst.klik_nrampe)
+        driver.execute_script("arguments[0].click();", nrampe)
 
-    # obrada dobijenih podataka - izdvajanje pojedinacnih vremena
-    pano_h = [(n, x.text) for n, x in enumerate(pvoznje)]
-    pano_km = [(m, y.text) for m, y in enumerate(pkilometara)]
+        # ocitavanje svih vremena predlozenih putanja u levom panou
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, pst.trajanje)))
 
-    # trazi se najduzi/najkraci put
-    mins = float(pano_km[0][1][:pano_km[0][1].find(' ')])
-    if pano_km[0][1].find(' km') == -1:
-        mins /= 1000
-    minm = 0
+        putanje = driver.find_elements(By.XPATH, "//*[starts-with(@id,'section-directions-trip')]/div/div/div/div/div")
 
-    for m, t in pano_km:
-        d = float(t[:t.find(' ')])
-        if t.find('km') == -1:
-            d /= 1000
+        zeljenput = km_m(putanje[0].text)
+        nput = 0
+        for i in range(1, len(putanje)):
+            trenutniput = km_m(putanje[i].text)
+            if kakvu == 'max':
+                if trenutniput > zeljenput:
+                    nput = i
+            elif kakvu == 'min':
+                if trenutniput < zeljenput:
+                    nput = i
 
-        if cilj == 'k':
-            if d < mins:
-                mins = d
-                minm = m
-        elif cilj == 'd':
-            if d > mins:
-                mins = d
-                minm = m
+        najb_km = putanje[nput].text
 
-    # najduzi/najkraci put je pod rednim brojem minm:
-    najb_h = pano_h[minm][1]
-    najb_km = pano_km[minm][1]
-
-    # klik na najduzi/najkraci put
-
-    driver.find_elements(By.XPATH, pst.pano)[minm].click()
-    if driver.find_element(By.XPATH, pst.detalji).is_displayed():
+        if not driver.find_element(By.XPATH, pst.pano).get_attribute('ID') == 'section-directions-trip-details-msg-' \
+                + str(nput):
+            driver.find_elements(By.XPATH, pst.pano)[nput].click()
         driver.find_element(By.XPATH, pst.detalji).click()
 
-    # pokupi vreme i kilometrazu za odabranu deonicu
-    time.sleep(2)
-    deo_h = driver.find_element(By.XPATH, pst.vr_deo).text
-    deo_km = driver.find_element(By.XPATH, pst.km_deo).text.replace('(', '').replace(')', '')
+        # pokupi vreme i kilometrazu za odabranu deonicu
+        time.sleep(5)
+        deo_km = driver.find_element(By.XPATH, pst.km_deo).text.replace('(', '').replace(')', '')
 
-    assert najb_h == deo_h, 'nisu ista vremena'
-    assert najb_km == deo_km, 'nije ista duzina'
+        # assert najb_h == deo_h, 'nisu ista vremena'
+        assert najb_km == deo_km, 'nije ista duzina'
 
-    driver.quit()
+        # driver.quit()
 
-except NoSuchElementException as exception:
-    print("Trazeni element " + exception.msg[1 + exception.msg.find(':'):exception.msg.find(
-        ';')] + " nema na ovom sajtu. Nesto si se prejebao.")
-    driver.quit()
+    except NoSuchElementException as exception:
+        print("Trazeni element " + exception.msg[1 + exception.msg.find(':'):exception.msg.find(
+            ';')] + " nema na ovom sajtu. Nesto si se prejebao.")
+        driver.quit()
 
-except TimeoutException:
-    print('Sajt je predugo ucitava. Probaj ponovo.')
-    driver.quit()
+    except TimeoutException:
+        print('Sajt je predugo ucitava. Probaj ponovo.')
+        driver.quit()
---
+++ main.py
@@ -0,0 +1,13 @@
+import Gmape
+
+od = 'Trieste'
+do = 'Beograd'
+trazi = 'min'
+
+
+def main():
+    Gmape.nadji_putanju(od, do, trazi)
+
+
+if __name__ == '__main__':
+    main()
